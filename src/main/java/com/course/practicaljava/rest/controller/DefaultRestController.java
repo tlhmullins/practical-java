6dc3f2b1abeca6ca73f9aef7139e0e0024afcd85
tlhullins <tlhmullins@yahoo.com>
2020-04-03 18:06:58 -0400

    Add logging
    Author updates

--- src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
@@ -2,6 +2,9 @@ package com.course.practicaljava.rest.controller;
 
 import java.time.LocalTime;
 
+import org.apache.tomcat.util.buf.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DefaultRestController {
 
+	private Logger log = LoggerFactory.getLogger(DefaultRestController.class);
+
 	@GetMapping("/welcome")
 	public String welcome() {
+
+		String[] strArray = { "Hello", "This is ", "Spring Boot ", "REST API" };
+		log.info(StringUtils.join(strArray));
+
 		return "Welcom to Spring Boot..it's going to be GREAT";
 	}
 
