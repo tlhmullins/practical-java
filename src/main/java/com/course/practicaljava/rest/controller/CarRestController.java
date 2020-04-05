177db60e4a0be2b733ace605511b62de4990fda1
tlhullins <tlhmullins@yahoo.com>
2020-04-05 17:53:35 -0400

    Commit car changes
    new attributes

--- src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
@@ -1,8 +1,12 @@
 package com.course.practicaljava.rest.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +20,20 @@ public class CarRestController {
 	@Autowired
 	private CarService carService;
 
+	private Logger log = LoggerFactory.getLogger(CarRestController.class);
+
 	@GetMapping(path = "/random", produces = MediaType.APPLICATION_JSON_VALUE)
 	// @GetMapping("/random")
 	public Car random() {
 
 		return carService.generateCar();
+	}
+
+	@PostMapping(path = "/echo", consumes = MediaType.APPLICATION_JSON_VALUE)
+	private String echo(@RequestBody Car car) {
+
+		log.info("The car is " + car);
+
+		return car.toString();
 	}
 }
--- src/main/java/com/course/practicaljava/rest/domain/Car.java
+++ src/main/java/com/course/practicaljava/rest/domain/Car.java
@@ -1,17 +1,30 @@
 package com.course.practicaljava.rest.domain;
 
+import java.util.Date;
+
 public class Car {
+
+	public Car() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	public Car(String brand, String color, String type) {
 		super();
 		this.brand = brand;
 		this.color = color;
 		this.type = type;
+		this.available = available;
+		this.price = price;
+
 	}
 
 	private String brand;
 	private String color;
 	private String type;
+	private int price;
+	private boolean available;
+	private Date firstReleaseDate;
 
 	public String getBrand() {
 		return brand;
@@ -39,7 +52,32 @@ public class Car {
 
 	@Override
 	public String toString() {
-		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + "]";
+		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + ", price=" + price + ", available="
+				+ available + ", firstReleaseDate=" + firstReleaseDate + "]";
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public Date getFirstReleaseDate() {
+		return firstReleaseDate;
+	}
+
+	public void setFirstReleaseDate(Date firstReleaseDate) {
+		this.firstReleaseDate = firstReleaseDate;
 	}
 
 }
